3a343d649a185c62e56c5331033b97a513884851
Dhruv Mehra <dhruvmehra2022@u.northwestern.edu>
2020-07-07 19:27:34 -0500

    updating timer js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("July 1, 2020 19:00:00").getTime();
+var countDownDate = new Date("July 25, 2020 17:22:00").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
 // Get today's date and time
