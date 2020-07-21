c9d80e74220952766912da56271252e2ac44dea0
Dhruv Mehra <dhruvmehra2022@u.northwestern.edu>
2020-07-21 14:51:06 -0500

    updated home and thanks

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("July 25, 2020 17:22:00").getTime();
+var countDownDate = new Date("July 25, 2020 17:22").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
 // Get today's date and time
@@ -13,28 +13,42 @@ var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 const template =`
-  <div class="countdown">
-    <div class="count">
-      <p class="number">${days}</p>
-      <p class="unit">DAYS</p>
+  <section class="countdown">
+    <p>premiere begins in...</p>
+    <div class="counter">
+      <div class="count">
+        <p class="number">${days}</p>
+        <p class="unit">DAYS</p>
+      </div>
+      <div class="count">
+        <p class="number">${hours}</p>
+        <p class="unit">HOURS</p>
+      </div>
+      <div class="count">
+        <p class="number">${minutes}</p>
+        <p class="unit">MINUTES</p>
+      </div>
+      <div class="count">
+        <p class="number">${seconds}</p>
+        <p class="unit">SECONDS</p>
+      </div>
     </div>
-    <div class="count">
-      <p class="number">${hours}</p>
-      <p class="unit">HOURS</p>
+  </section>`;
+
+  const enter =`
+  <div class="braff">
+    <p>Welcome to Premiere!</p>
+    <p class="zach">a message from Zach Braff  (SoC '97)</p>
+    <div class="braff" style="padding:56.25% 0 0 0;position:relative;">
+      <iframe src="https://player.vimeo.com/video/439650699?color=75ffeb&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
     </div>
-    <div class="count">
-      <p class="number">${minutes}</p>
-      <p class="unit">MINUTES</p>
-    </div>
-    <div class="count">
-      <p class="number">${seconds}</p>
-      <p class="unit">SECONDS</p>
-    </div>
+    <script src="https://player.vimeo.com/api/player.js"></script>
+    <div class="nextFilm"><a href="boba.html">ENTER PREMIERE</a></div>
   </div>`;
 
 document.getElementById("demo").innerHTML = template;
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "ENTER";
+    document.getElementById("demo").innerHTML = enter;
   }
 }, 1000);
